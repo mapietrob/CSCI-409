9b132d9c4299d4e9ef4e94c3acb4504487d3d1bb
mapietrob <mapietrob@coastal.edu>
2024-02-07 01:53:53 -0500

    Created core app

--- api/api/settings.py
+++ api/api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,10 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST' : os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER' : os.environ.get('DE_USER'),
+        'PASSWORD' : os.environ.get('DB_PASS'),
     }
 }
 
